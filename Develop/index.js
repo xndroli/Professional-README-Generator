a0df1ec0b5558d9169b574d3f8ba0ef66a0efff7
xndroli <iamsimplyclean@knights.ucf.edu>
2022-06-19 23:54:14 -0400

    init commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,144 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What's the title of your project?",
+        validate(answer) {
+            if (answer.length > 1) {
+                return true;
+            }
+            return 'Please enter a valid project title';
+        },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What's your project description?",
+        validate(answer) {
+            if (answer.length > 1) {
+                return true;
+            }
+    
+            return 'Please enter a valid project description';
+        },
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What are your installation instructions?",
+        validate(answer) {
+            if (answer.length > 1) {
+                return true;
+            }
+    
+            return 'Please list installation instructions for your application';
+        },
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What's your project application usage?",
+        validate(answer) {
+            if (answer.length > 1) {
+                return true;
+            }
+    
+            return 'Please explain instructions for using your application';
+        },
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        validate(answer) {
+            if (answer.length = 1) {
+                return true;
+            }
+    
+            return 'Please choose a license for your project';
+        },
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: "Who are your project contributors?",
+        validate(answer) {
+            if (answer.length > 1) {
+                return true;
+            }
+    
+            return 'Please list who contributed to your project';
+        },
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: "What's your GitHub username?",
+        validate(answer) {
+            if (answer.length > 1) {
+                return true;
+            }
+    
+            return 'Please enter your Github username';
+        },
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What's your email address?",
+        validate(answer) {
+            if (answer.length > 1) {
+                return true;
+            }
+    
+            return 'Please enter your email address';
+        },
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log('Oops.. An error occured: ' + err);
+        }
+        console.log("Success! Your README.md file has been generated")
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const writeFileAsync = util.promisify(writeToFile);
+async function init() {
+    try {
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (err) {
+        console.log('Oops.. An error occured: ' + err);
+    }
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+  
+  ## Table of Contents 
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseTOC(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## [Description](#table-of-contents)
+  ${data.description}
+
+  ## [Installation](#table-of-contents)
+  ${data.installation}
+
+  ## [Usage](#table-of-contents)
+  ${data.usage}
+
+  ## [License](#table-of-contents)
+  ${data.licensing}
+
+  ## [Contribution](#table-of-contents)
+  ${data.contribution}
+
+  ## [Testing](#table-of-contents)
+  ${data.testing}
+
+  ## [Questions](#table-of-contents)
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} 
 `;
 }
 
