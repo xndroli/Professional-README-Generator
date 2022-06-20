e036a41a608c39e732fd021286dbbec617044c36
xndroli <iamsimplyclean@knights.ucf.edu>
2022-06-19 23:59:45 -0400

    updated markdown logic

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
-}
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
-}
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
-}
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -22,14 +42,14 @@ function generateMarkdown(data) {
   # ${data.title}
 
   ## Licensing:
-  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+  ${renderLicenseBadge(data.license)}
   
   ## Table of Contents 
 
   * [Description](#description)
   * [Installation](#installation)
   * [Usage](#usage)
-  ${renderLicenseTOC(data.license)}
+  ${renderLicenseSection(data.license)}
   * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
@@ -44,7 +64,7 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## [License](#table-of-contents)
-  ${data.licensing}
+  ${renderLicenseSection(data.license)}}
 
   ## [Contribution](#table-of-contents)
   ${data.contribution}
